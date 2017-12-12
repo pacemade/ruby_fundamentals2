ea68742a4a8c1ce7eec9af29673255696c0deaf4
Danny Chang <dannychachang@gmail.com>
2017-12-12 15:16:03 -0500

    exercise 2, negative?

--- ruby_fundamentals2.rb
+++ ruby_fundamentals2.rb
@@ -3,3 +3,13 @@
 def double(my_number)
   my_number * 2
 end
+
+# Exercise 2 --------------------------------------------
+
+def negative?(neg)
+  if neg < 0
+   return true
+  else
+   return false
+  end
+end
